bb62bb0585260e961a1e917101d0de67447d1143
Suryansh015 <suryanshhimalayan@gmail.com>
2025-02-20 04:55:40 +0000

    a

--- authentication-backend/src/db.ts
+++
@@ -1,9 +0,0 @@
-import mongoose  from "mongoose";
-
-const userschema  = new mongoose.Schema({
-    userName  : String , 
-    email : String  , 
-    password : String
-})
-
-export const userModel = mongoose.model("user" , userschema) ; 
